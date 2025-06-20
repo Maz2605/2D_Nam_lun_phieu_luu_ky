79e2b698f27921e7acd07923c10c48cc3989f7a1
Maz2605 <vannam2654@gmail.com>
2025-06-20 19:57:06 +0700

    Test _ 3

--- Assets/_Scripts/Enemy/Enemies/Skull/Skull.cs
+++ Assets/_Scripts/Enemy/Enemies/Skull/Skull.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Skull : MonoBehaviour
 {
-   
+    private void FixedUpdate()
+    {
+        
+    }
 }
